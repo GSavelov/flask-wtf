4c2e4af5d51d506a89899f5eecb7e6e5f9800408
savelov <mpxlroyal@gmail.com>
2024-02-12 18:26:06 +0500

    Task №2

--- main.py
+++ main.py
@@ -9,5 +9,10 @@ def index(title):
     return render_template('index.html', title=title)
 
 
+@app.route('/training/<prof>')
+def training(prof):
+    return render_template('index.html', prof=prof, title="Тренировка")
+
+
 if __name__ == '__main__':
     app.run(port=8080)
